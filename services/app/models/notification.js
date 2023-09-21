70251e8adf7d9922465af11deb37157f02420a1b
hiJes <gitaadj97@gmail.com>
2023-09-22 00:57:21 +0700

    chore: migration for notifications

---
+++ services/app/models/notification.js
@@ -0,0 +1,42 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Notification extends Model {
+
+    static associate(models) {
+      // define association here
+    }
+  }
+  Notification.init({
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Title is required!"
+        },
+        notNull: {
+          msg: "Title is required!"
+        }
+      }
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Description is required!"
+        },
+        notNull: {
+          msg: "Description is required!"
+        }
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Notification',
+  });
+  return Notification;
+};
