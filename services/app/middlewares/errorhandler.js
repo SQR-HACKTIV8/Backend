4dba78aa1af7b780e819f28152cddf18e7e679ae
Fatahillah <fatahillah@yandex.com>
2023-09-22 13:20:00 +0700

    feat: migration, seeding, validation, CRU for Qurban and CR for Category entity

--- services/app/middlewares/errorhandler.js
+++ services/app/middlewares/errorhandler.js
@@ -1,9 +1,10 @@
-function errorHandler (err, req, res, next) {
-  let status = 500
-  let message = "Internal Server Error"
+function errorHandler(err, req, res, next) {
+  let status = 500;
+  let message = "Internal Server Error";
 
   console.log(err, "<< Error handler App");
 
-  res.status(status).json({message})
+  res.status(status).json({ message });
 }
-module.exports = errorHandler
+
+module.exports = errorHandler;
--- services/app/routes/index.js
+++ services/app/routes/index.js
@@ -1,7 +1,21 @@
-const router = express().Router
+const express = require("express");
+const router = express().Router;
+const Controller = require("../controllers/controller");
+const errorHandler = require("../middlewares/errorhandler");
 
-module.exports = router
+router.get("/", (req, res) => {
+  res.send("Hello from App-services!");
+});
 
-router.get('/', (req, res) => {
-  res.send('Hello from App-services!')
-})
+router.get("/categories", Controller.showAllCategories);
+router.post("/categories", Controller.addCategory);
+
+router.get("/qurbans", Controller.showAllQurbans);
+router.get("/qurbans", Controller.showAllQurbans);
+router.post("/qurbans", Controller.addQurban);
+router.get("/qurbans/:id", Controller.showDetailQurban);
+router.patch("/qurbans/:id", Controller.updateQurban);
+
+router.use(errorHandler);
+
+module.exports = router;
